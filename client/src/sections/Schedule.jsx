87b05905a5da18135a370fdf0bfee3bbcd1a3d21
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-08-25 14:37:02 +0530

    Add locations and filter divs

--- client/src/sections/Schedule.jsx
+++ client/src/sections/Schedule.jsx
@@ -1,20 +1,30 @@
 const Schedule = () => {
   return (
-    <div className="px-24 py-20">
-      <h1 className="font-darker-grotesque text-6xl text-start font-medium">
-        SCHEDULE
-      </h1>
+    <div className="px-24 py-20 font-darker-grotesque">
+      <h1 className=" text-6xl text-start font-semibold">SCHEDULE</h1>
 
-      <div>
+      <div className="flex py-16 justify-around text-3xl font-semibold">
         <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div>Dhwani Stage</div>
+        <div>DJ Hall</div>
+        <div>CETAA Hall</div>
       </div>
 
-      <div>
-        <div></div>
-        <div></div>
+      <div className="flex text-2xl font-medium">
+        <div className="flex-1 border-solid border-2 border-red-700 "></div>
+        <div className="border-solid border-2 border-red-700 w-3/12 pt-10 pl-10">
+          Filter By:
+          <div className="leading-6">
+            <div className>
+              <input type="radio" className="align-middle" name="filter-by" />{" "}
+              Venue
+            </div>
+            <div>
+              <input type="radio" className="align-middle" name="filter-by" />{" "}
+              Type
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
