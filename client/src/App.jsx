15cef65349b255ad99bec913b6bc38e9b1b7fa01
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-08-25 11:16:38 +0530

    Add Schedule page layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
+import Schedule from "./sections/Schedule";
 import Speakers from "./sections/Speakers";
 
 function App() {
   return (
     <div>
       <Speakers />
+      <Schedule />
     </div>
   );
 }
---
+++ client/src/sections/Schedule.jsx
@@ -0,0 +1,23 @@
+const Schedule = () => {
+  return (
+    <div className="px-24 py-20">
+      <h1 className="font-darker-grotesque text-6xl text-start font-medium">
+        SCHEDULE
+      </h1>
+
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Schedule;
