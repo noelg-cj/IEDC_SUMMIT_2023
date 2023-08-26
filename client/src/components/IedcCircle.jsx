717966c9eb25ab00804258ed4113f6a7d0c24d67
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-08-26 18:45:46 +0530

    Add Summit Icon

--- client/src/components/IedcCircle.jsx
+++ client/src/components/IedcCircle.jsx
@@ -1,8 +1,10 @@
 import IedcSubCircle from "./IedcSubCircle";
+import summitIcon from "../assets/img/summitIcon.svg";
 
 const IedcCircle = () => {
   return (
     <div>
+      <img src={summitIcon} className="w-48 sm:w-72 lg:w-96 round-full" />
       <IedcSubCircle />
     </div>
   );
