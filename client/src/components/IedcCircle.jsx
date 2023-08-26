205697daab6178c293b24be04d526431d16c019e
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-08-26 18:51:28 +0530

    Link datas from data.js

--- client/src/components/IedcCircle.jsx
+++ client/src/components/IedcCircle.jsx
@@ -1,11 +1,14 @@
 import IedcSubCircle from "./IedcSubCircle";
 import summitIcon from "../assets/img/summitIcon.svg";
+import { iedcCircle } from "../data";
 
 const IedcCircle = () => {
   return (
     <div>
       <img src={summitIcon} className="w-48 sm:w-72 lg:w-96 round-full" />
-      <IedcSubCircle />
+      {iedcCircle.map((sub, index) => (
+        <IedcSubCircle datas={sub} key={sub.title + index} />
+      ))}
     </div>
   );
 };
