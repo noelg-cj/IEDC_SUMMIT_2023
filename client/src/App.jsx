2078745aaccc8f0485a465f65b8b3be51b209300
Noel George C J <noelgeorge2004@gmail.com>
2023-08-26 16:12:50 +0530

    Added about summit section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import Speakers from "./sections/Speakers";
 import Events from "./sections/Events";
 import CountDown from "./components/countDown";
+import AboutSummit from "./sections/AboutSummit";
 function App() {
   return (
     <div className=" w-full min-h-screen flex flex-col items-center">
-      
       <CountDown/>
+      <AboutSummit />
       <Speakers/>
       <Events/>
     </div>
