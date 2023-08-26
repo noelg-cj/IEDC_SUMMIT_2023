ab86b70d067268fc5042cc940819c95712de3aaa
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-08-26 18:32:42 +0530

    Improved SubCircel responsiveness

--- client/src/components/IedcSubCircle.jsx
+++ client/src/components/IedcSubCircle.jsx
@@ -1,9 +1,11 @@
 const IedcSubCircle = () => {
   return (
-    <div className="bg-gradient-to-br from-[#0E79BE] to-[#17C4E8] p-1 rounded-full w-40">
-      <div className=" pb-2 w-full bg-white rounded-full aspect-square flex flex-col justify-center items-center">
-        <div className=" text-7xl font-bold text-[#2880B9]">14</div>
-        <div className="text-xl font-semibold uppercase text-[#605D5D]">
+    <div className="font-dm-sans bg-gradient-to-br from-[#0E79BE] to-[#17C4E8] p-1 rounded-full w-24 sm:w-32 lg:w-44">
+      <div className="w-full bg-white rounded-full aspect-square flex flex-col justify-center items-center">
+        <div className="text-2xl sm:text-4xl lg:text-7xl font-bold text-[#2880B9]">
+          14+
+        </div>
+        <div className="text-xs sm:text-lg lg:text-xl font-semibold uppercase text-[#605D5D]">
           Colleges
         </div>
       </div>
