ec4fce613ab0559811a7c7b1ba82082a4271a843
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-08-26 14:07:17 +0530

    Center schedule tile number

--- client/src/components/ScheduleTile.jsx
+++ client/src/components/ScheduleTile.jsx
@@ -7,21 +7,21 @@ const ScheduleTile = ({ datas, num, top, bottom }) => {
         {datas.time}
       </div>
       <div className="w-2 relative self-stretch my-[-20px] flex flex-col justify-center mr-4">
-        <div className="text-2xl font-semibold text-white bg-theme-blue3 w-8 h-8 text-center rounded-full z-10">
+        <div className="text-2xl font-semibold text-white bg-theme-blue3 w-9 h-6 pb-9 text-center rounded-full z-10">
           {num}
         </div>
         <div
           className={
             top
               ? "absolute opacity-0"
-              : "absolute bg-theme-blue3 w-1 h-4/6 left-3.5 top-[-10%]"
+              : "absolute bg-theme-blue3 w-1 h-4/6 left-4 top-[-10%]"
           }
         ></div>
         <div
           className={
             bottom
               ? "absolute opacity-0"
-              : "absolute bg-theme-blue3 w-1 h-4/6 left-3.5 bottom-[-10%]"
+              : "absolute bg-theme-blue3 w-1 h-4/6 left-4 bottom-[-10%]"
           }
         ></div>
       </div>
