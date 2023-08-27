94ac5aeec1cff39a8bd36c5cfbdde4da6ae169ba
aharonmathews <aharonmathews04@gmail.com>
2023-08-27 15:42:57 +0530

    Map

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import Speakers from "./sections/Speakers";
 import Events from "./sections/Events";
 import CountDown from "./components/countDown";
+import Directions from "./sections/Direction";
 function App() {
   return (
     <div className=" w-full min-h-screen flex flex-col items-center">
@@ -8,6 +9,7 @@ function App() {
       <CountDown/>
       <Speakers/>
       <Events/>
+      <Directions/ >
     </div>
   );
 }
