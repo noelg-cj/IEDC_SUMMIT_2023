fd2910897996a4533d0c1470d5f1d911fb1f7c4d
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-08-29 14:32:25 +0530

    Fix landingpage z-index issue

--- client/src/sections/landingPage.jsx
+++ client/src/sections/landingPage.jsx
@@ -6,7 +6,7 @@ import bg2 from "../assets/img/about/C2.png";
 function LandingPage() {
   return (
     <div className="relative w-full bg- flex flex-col pb-12">
-      <img src={bg0} className="absolute right-0" />
+      <img src={bg0} className="absolute right-0 -z-50" />
 
       <div className="absolute left-0 right-0 flex flex-col items-start w-72 md:w-[22rem] lg:w-auto -z-50">
         <img src={bg1} className="mb-[-5px]" />
@@ -21,7 +21,7 @@ function LandingPage() {
           2023
         </h>
         <h className="font-bold text-2xl md:text-5xl text-[#0065B2] pb-4">
-          OCTOBER XX
+          OCTOBER 12
         </h>
         <h className="font-semibold text-xl md:text-3xl text-[#676C70] pb-1">
           COLLEGE OF ENGINEERING,
@@ -29,8 +29,8 @@ function LandingPage() {
         <h className="font-semibold text-xl md:text-3xl text-[#676C70] pb-16">
           TRIVANDRUM
         </h>
-        <div className="flex  items-center w-[232px] h-[58px] bg-[#097ED8] rounded-[30px]">
-          <div className="font-bold text-white text-2xl justify-center mx-auto">
+        <div className="flex  items-center w-[232px] h-[58px] bg-[#097ED8] rounded-[30px] ">
+          <div className="font-bold text-white text-2xl justify-center mx-auto cursor-pointer">
             BUY TICKETS
           </div>
         </div>
