6535cba12ccc82a290f2fbe9d574fde5aac0123f
Ivin <ivinmk2410@hotmail.com>
2023-09-07 23:26:21 +0530

    Rounded edge for team modal box

--- client/src/components/TeamCard.jsx
+++ client/src/components/TeamCard.jsx
@@ -2,16 +2,16 @@ import React from "react";
 
 function TeamCard({ member }) {
   return (
-    <div className="bg-gradient-to-br from-[#0597F2] to-[#6F04D9] p-1 rounded-[70px]">
+    <div className="bg-gradient-to-br from-[#0597F2] to-[#6F04D9] p-1 rounded-[70px] padding-[10px]">
       <div className="w-full bg-white rounded-[70px] flex gap-4">
         <div className="w-[130px] h-[130px] rounded-full flex-none p-2">
           <img src={member.image} alt={member.name} className="w-full h-full rounded-full" />
         </div>
         <div className="flex-1 flex flex-col justify-center items-start">
-          <p className="text-2xl font-semibold">
+          <p className="text-xl lg:text-xl xl:text-2xl  font-semibold">
             {member.name}
           </p>
-          <p className="text-xl lg:text-xl xl:text-2xl font-[300]">
+          <p className="text-l lg:text-l xl:text-xl font-[300]">
             {member.role}
           </p>
         </div>
--- client/src/components/TeamModalbox.jsx
+++ client/src/components/TeamModalbox.jsx
@@ -5,13 +5,8 @@ import TeamCard from "../components/TeamCard";
 function Team() {
   console.log("hello");
   return (
-    <div className="relative font-dm-sans  flex flex-col overflow-auto " style={{height: '75vh'}}>
-      <div className="w-11/12 space-y-6 text-center self-center flex flex-col bg-[#C9D9E8]" >
-        <h1 className="inline font-[700] text-[75px] bg-gradient-to-tr from-[#0597F2] to-[#6F04D9] bg-clip-text text-transparent">
-        Team
-        </h1>
-      </div>
-      <div className="sm:grid lg:grid-cols-3 sm:grid-cols-2 flex flex-col gap-10" >
+    <div className="relative font-dm-sans  flex flex-col overflow-auto  " style={{height: '60vh'}}>
+      <div className="sm:grid xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 flex flex-col gap-8" >
         {TeamMembers.map((member, index) => (
           <TeamCard member={member} key={index} />
         ))}
@@ -23,10 +18,15 @@ function Team() {
 export default function Modal(props) {
   return (
     <div className="static ">
-      <div className="fixed top-20 right-20 left-20 z-20 flex justify-center ">
+      <div className="fixed top-28 right-36 left-36 z-20 flex justify-center bg-[#C9D9E8] p-1 rounded-[65px]">
           <div className="mx-4 my-4 bg-[#C9D9E8]">
               <div className="flex justify-end bg-[#C9D9E8]">
-                  <button onClick={() => props.toggler()} className="border-2 text-red-900 px-2 m-2">
+                <div className="w-11/12 space-y-6 text-center self-center flex flex-col bg-[#C9D9E8] " >
+                  <h1 className="inline font-[700] text-[65px] bg-gradient-to-tr from-[#0597F2] to-[#6F04D9] bg-clip-text text-transparent">
+                  Team
+                  </h1>
+                </div>
+                  <button onClick={() => props.toggler()} className="text-red-900 px-2 m-2 ">
                     X
                   </button>
               </div>
