b3450ba799c1c63e943d79ae23e41904f9b94b8d
Ivin <ivinmk2410@hotmail.com>
2023-09-08 20:44:29 +0530

    Modal box design and response

--- client/src/components/TeamCard.jsx
+++ client/src/components/TeamCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function TeamCard({ member }) {
   return (
-    <div className="bg-gradient-to-br from-[#0597F2] to-[#6F04D9] p-1 rounded-[70px] padding-[10px]">
+    <div className="bg-gradient-to-br from-[#0597F2] to-[#6F04D9] p-1 rounded-[70px]">
       <div className="w-full bg-white rounded-[70px] flex gap-4">
         <div className="w-[130px] h-[130px] rounded-full flex-none p-2">
           <img src={member.image} alt={member.name} className="w-full h-full rounded-full" />
--- client/src/components/TeamModalbox.jsx
+++ client/src/components/TeamModalbox.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { TeamMembers } from "../data";
 import TeamCard from "../components/TeamCard";
+import "../assets/css/team.css";
 
 function Team() {
   console.log("hello");
   return (
-    <div className="relative font-dm-sans  flex flex-col overflow-auto  " style={{height: '60vh'}}>
-      <div className="sm:grid xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 flex flex-col gap-8" >
+    <div className="relative font-dm-sans  flex flex-col overflow-auto  " style={{height: "55vh"}}>
+      <div className="sm:grid xl:grid-cols-3 lg:grid-cols-2 md:grid-cols-2 flex flex-col gap-8 padding" >
         {TeamMembers.map((member, index) => (
           <TeamCard member={member} key={index} />
         ))}
@@ -18,11 +19,11 @@ function Team() {
 export default function Modal(props) {
   return (
     <div className="static ">
-      <div className="fixed top-28 right-36 left-36 z-20 flex justify-center bg-[#C9D9E8] p-1 rounded-[65px]">
+      <div className="fixed top-[20%] lg:right-[10%] lg:left-[10%] right-[6%] left-[6%] sm:z-20 flex justify-center bg-[#C9D9E8] p-1 rounded-[40px] md:rounded-[65px] sm:rounded-[50px]">
           <div className="mx-4 my-4 bg-[#C9D9E8]">
               <div className="flex justify-end bg-[#C9D9E8]">
                 <div className="w-11/12 space-y-6 text-center self-center flex flex-col bg-[#C9D9E8] " >
-                  <h1 className="inline font-[700] text-[65px] bg-gradient-to-tr from-[#0597F2] to-[#6F04D9] bg-clip-text text-transparent">
+                  <h1 className="inline font-[700] text-[55px] bg-gradient-to-tr from-[#0597F2] to-[#6F04D9] bg-clip-text text-transparent">
                   Team
                   </h1>
                 </div>
