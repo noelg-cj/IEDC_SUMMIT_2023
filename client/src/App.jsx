debef90e6c3919c45df543bc540ded95d04443ea
john-George510 <meetjohn510@gmail.com>
2023-09-22 13:06:49 +0530

    router chnages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,16 @@
-import MainPage from "./pages/mainPage"
+import MainPage from "./pages/mainPage";
 import LeaderboardPage from "./pages/leaderBoard";
-import { BrowserRouter as Router, Route, Routes,createBrowserRouter,createRoutesFromElements,RouterProvider } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
-  
   return (
     <Router>
-    <Routes>
-      <Route path="/" element={<MainPage/>}/>
-      <Route path="/leaderboard" element={<LeaderboardPage/>}/>
-    </Routes>
-  </Router>
-    
+      <Routes>
+        <Route exact path="/" element={<MainPage />} />
+        <Route exact path="/leaderboard" element={<LeaderboardPage />} />
+      </Routes>
+    </Router>
   );
 }
-// const router = createBrowserRouter(
-// 	createRoutesFromElements(
-//     <Routes>
-//       <Route path="/" element={<MainPage />}/>
-//         <Route path="/leaderboard" element={ <LeaderboardPage />} errorElement={<h2>error from auth</h2>}/>
-//       {/* </Route> */}
-//     // </Routes>
-// 	)
-// );
 
-// function App(){
-// 	return <RouterProvider router={router} />;
-	
-// };
 export default App;
