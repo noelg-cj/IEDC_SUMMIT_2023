5de72f6ef2cd53a0f60f5678f82bd1432fbf77eb
john-George510 <meetjohn510@gmail.com>
2023-09-22 12:56:54 +0530

    removed wouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,31 @@
 import MainPage from "./pages/mainPage"
 import LeaderboardPage from "./pages/leaderBoard";
-import { Route, Router } from 'wouter';
+import { BrowserRouter as Router, Route, Routes,createBrowserRouter,createRoutesFromElements,RouterProvider } from 'react-router-dom';
 
-const App = () => {
+function App() {
+  
   return (
     <Router>
-      <Route path="/" component={MainPage} />
-      <Route path="/leaderboard" component={LeaderboardPage} />
-    </Router>
+    <Routes>
+      <Route path="/" element={<MainPage/>}/>
+      <Route path="/leaderboard" element={<LeaderboardPage/>}/>
+    </Routes>
+  </Router>
+    
   );
-};
+}
+// const router = createBrowserRouter(
+// 	createRoutesFromElements(
+//     <Routes>
+//       <Route path="/" element={<MainPage />}/>
+//         <Route path="/leaderboard" element={ <LeaderboardPage />} errorElement={<h2>error from auth</h2>}/>
+//       {/* </Route> */}
+//     // </Routes>
+// 	)
+// );
 
+// function App(){
+// 	return <RouterProvider router={router} />;
+	
+// };
 export default App;
