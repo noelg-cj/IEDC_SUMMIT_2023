fa2a188dbcc66d13a470a89e4bb4a52717f2a57e
Aslam-Naseer <aslamnaseer22@gmail.com>
2023-09-25 22:02:50 +0530

    Add Scroll in Events mobile

--- client/src/components/Tickets.jsx
+++ client/src/components/Tickets.jsx
@@ -15,12 +15,17 @@ function Ticket(props) {
   }
 
   return (
-    <div className=" mb-6  overflow-hidden relative flex flex-col h-[622px] 
+    <div
+      className="flex-none mb-6  overflow-hidden relative flex flex-col h-[622px] 
     bg-[#3866F2] hover:bg-gradient-to-br from-[#1687F2] to-[#5D25E1]
     w-[365.82px] rounded-[30px]"
     >
       <div className="h-[234px] bg-white  self-center m-[19px]  w-11/12 rounded-[30px]">
-        <img src={eventImg} alt="mathew" className="rounded-[30px] w-full h-full " />
+        <img
+          src={eventImg}
+          alt="mathew"
+          className="rounded-[30px] w-full h-full "
+        />
       </div>
       <div className="self-center flex flex-row bg-lime- ">
         <div className="w-[38px] rotate-180  overflow-hidden">
@@ -33,25 +38,20 @@ function Ticket(props) {
       </div>
       <div className="font-dm-sans">
         <div className="w-11/12 mx-auto  h-fit items-center  mx-auto">
-              <h1 className="font-[900] text-3xl leading-tight">
-                {eventName}
-              </h1>
-          <p className=" font-[300] text-[18px]">
-          {eventDescription}
-          </p>
+          <h1 className="font-[900] text-3xl leading-tight">{eventName}</h1>
+          <p className=" font-[300] text-[18px]">{eventDescription}</p>
         </div>
         <div className="justify-center flex">
-        {eventLink !== '' ? (
-          <a
-            href={eventLink}
-            target="_blank"
-            className="text-center font-[700] absolute bottom-5 text-xl font-extrabold w-[151px] h-[60px] bg-[#FFFFFF] shadow-md rounded-[30px] flex items-center justify-center hover:bg-[#E1F3FE]"
-          >
-            {buttonTitle}
-          </a>
-        ) : null}
-
-
+          {eventLink !== "" ? (
+            <a
+              href={eventLink}
+              target="_blank"
+              className="text-center font-[700] absolute bottom-5 text-xl font-extrabold w-[151px] h-[60px] bg-[#FFFFFF] shadow-md rounded-[30px] flex items-center justify-center hover:bg-[#E1F3FE]"
+              rel="noreferrer"
+            >
+              {buttonTitle}
+            </a>
+          ) : null}
         </div>
       </div>
     </div>
--- client/src/sections/Events.jsx
+++ client/src/sections/Events.jsx
@@ -2,40 +2,38 @@ import React from "react";
 import Tickets from "../components/Tickets";
 
 function Events(props) {
-  const { title, button, sectionRef, eventData,eventDescription } = props;
-  
+  const { title, button, sectionRef, eventData, eventDescription } = props;
+
   return (
     <div
       ref={sectionRef}
       className="  relative space-y-10 font-dm-sans  lg:w-10/12 w-11/12  flex flex-col l"
-    ><div className="absolute overflow-hidden -z-10 opacity-5 text-[#0597F2] xl:top-[-100px] lg:top-[-70px] md:top-[-20px] hidden-xs top-0 w-full space-y-6 text-center self-center">
-    <h1 className="xl:text-[250px] lg:text-[200px] md:text-[150px] sm:text-[120px] whitespace-nowrap font-bold">
-      {title}
-    </h1>
-  </div>
+    >
+      <div className="absolute overflow-hidden -z-10 opacity-5 text-[#0597F2] xl:top-[-100px] lg:top-[-70px] md:top-[-20px] hidden-xs top-0 w-full space-y-6 text-center self-center">
+        <h1 className="xl:text-[250px] lg:text-[200px] md:text-[150px] sm:text-[120px] whitespace-nowrap font-bold">
+          {title}
+        </h1>
+      </div>
       <div className="w-11/12 space-y-6 text-center self-center">
         <h1 className=" font-[700] text-[75px] text-[#0065B2]">{title}</h1>
         <p className=" font-[400] text-[24px] text-[#676C70]">
           {eventDescription}
         </p>
       </div>
-      <div className="flex flex-row flex-wrap justify-evenly">
-  {eventData && eventData.length > 0 && eventData.map((ticket, index) => (
-    <Tickets
-      key={index}
-      buttonTitle={button}
-      eventName={ticket.ticketTitle}
-      eventImg={ticket.ticketImg}
-      eventDescription={ticket.ticketDescription}
-      eventLink={ticket.ticketLink}
-    />
-  ))}
-</div>
-
-
-
-
-
+      <div className="flex md:flex-wrap overflow-scroll scroll-smooth gap-8 md:gap-0 justify-evenly">
+        {eventData &&
+          eventData.length > 0 &&
+          eventData.map((ticket, index) => (
+            <Tickets
+              key={index}
+              buttonTitle={button}
+              eventName={ticket.ticketTitle}
+              eventImg={ticket.ticketImg}
+              eventDescription={ticket.ticketDescription}
+              eventLink={ticket.ticketLink}
+            />
+          ))}
+      </div>
     </div>
   );
 }
